eb07cb77b5d2755b6283ac0f895634c7f3567b97
Han Chen <han.chen795@gmail.com>
2023-02-11 19:03:42 -0800

    Initial commit

--- main.py
+++ main.py
@@ -11,16 +11,34 @@ def main():
     gg_seq, gg_header = read_fasta("./data/Gallus_gallus_BRD2.fa")
     mm_seq, mm_header = read_fasta("./data/Mus_musculus_BRD2.fa")
     br_seq, br_header = read_fasta("./data/Balaeniceps_rex_BRD2.fa")
-    tt_seq, tt_header = read_fasta("./data/tursiops_truncatus_BRD2.fa")
+    tt_seq, tt_header = read_fasta("./data/tursiops_truncatus_BRD2.fa")  
 
     # TODO Align all species to humans and print species in order of most similar to human BRD
     # using gap opening penalty of -10 and a gap extension penalty of -1 and BLOSUM62 matrix
-    pass
 
+    # Initialize NW
+    NW = NeedlemanWunsch("./substitution_matrices/BLOSUM62.mat",
+                         gap_open = -10,
+                         gap_extend = -1)
+    # Calculate alignment scores
+    gg_score, gg_hs, gg = NW.align(hs_seq, gg_seq)
+    mm_score, mm_hs, ms = NW.align(hs_seq, mm_seq)
+    br_score, br_hs, bs = NW.align(hs_seq, br_seq)
+    tt_score, tt_hs, tt = NW.align(hs_seq, tt_seq)
+
+    speices_scores = {"GG" : gg_score,
+                      "MM" : mm_score,
+                      "BR" : br_score,
+                      "TT" : tt_score}  
+    # Sort the species scores
+    species_scores_sorted = sorted(speices_scores.items(),
+                                   key=lambda x:x[1])
+    
     # TODO print all of the alignment score between each species BRD2 and human BRD2
     # using gap opening penalty of -10 and a gap extension penalty of -1 and BLOSUM62 matrix
-    pass
+    print("Alignment scores of species to human BRD2:")
+    for species, scores in species_scores_sorted:
+        print(species + ": " + str(scores))
     
-
 if __name__ == "__main__":
     main()
--- test/test_align.py
+++ test/test_align.py
@@ -14,7 +14,37 @@ def test_nw_alignment():
     """
     seq1, _ = read_fasta("./data/test_seq1.fa")
     seq2, _ = read_fasta("./data/test_seq2.fa")
-    pass
+
+    NW = NeedlemanWunsch("./substitution_matrices/BLOSUM62.mat",
+                         gap_open = -10,
+                         gap_extend = -1)
+    
+    score, seqA, seqB = NW.align(seq1, seq2)
+    assert score == 4
+    assert seqA == "MYQR"
+    assert seqB == "M-QR"
+
+    i = -np.inf
+    m = np.array([[0,   i,    i,   i],
+                  [i,   5,  -11,   13],
+                  [i, -12,    4,   -8],
+                  [i, -12,   -1,    5],
+                  [i, -14,   -6,    4]])
+    assert m.all() == NW._align_matrix.all()
+    
+    known_A = np.array([[-10,   i,    i,    i],
+                        [-11, -12,   -6,   -7],
+                        [-12, -13,  -14,   -7],
+                        [-13, -14,  -15,  -12],
+                        [-14, -15,  -16,  -17]])
+    assert known_A.all() == NW._gapA_matrix.all()
+
+    known_B = np.array([[-10, -11,  -12, -13],
+                        [i,   -12,  -13, -14],
+                        [i,    -6,  -14, -15],
+                        [i,    -7,   -7, -16],
+                        [i,    -8,   -8,  -6]])
+    assert known_B.all() == NW._gapB_matrix.all()
     
 
 def test_nw_backtrace():
@@ -27,7 +57,14 @@ def test_nw_backtrace():
     """
     seq3, _ = read_fasta("./data/test_seq3.fa")
     seq4, _ = read_fasta("./data/test_seq4.fa")
-    pass
+
+    NW = NeedlemanWunsch("./substitution_matrices/BLOSUM62.mat",
+                         gap_open = -10,
+                         gap_extend = -1)
+    
+    assert NW.align(seq3, seq4) == (17, 
+                                    'MAVHQLIRRP', 
+                                    'M---QLIRHP') 
 
 
 
